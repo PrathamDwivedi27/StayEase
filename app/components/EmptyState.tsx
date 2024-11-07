c86a9e14ddcdc2ab22b1865ad7d6a16211a5fd2f
PrathamDwivedi27 <prathamd549@gmail.com>
2024-11-07 19:46:43 +0530

    [Fix]: Fix the hydration error

---
+++ app/components/EmptyState.tsx
@@ -0,0 +1,31 @@
+"use client"
+import React from 'react'
+import Heading from './Heading';
+import { useRouter } from 'next/navigation';
+
+interface EmptyStateProps{
+    title?:string;
+    subtitle?:string;
+    showReset?:boolean
+}
+
+const EmptyState:React.FC<EmptyStateProps> = ({
+    title="No exact matches",
+    subtitle="Try changing or removing some of your filters",
+    showReset
+}) => {
+    const router=useRouter();
+  return (
+    <div 
+        className='h-[60vh] flex flex-col justify-center items-center gap-2'
+    >
+      <Heading
+        center
+        title={title}
+        subtitle={subtitle}
+      />
+    </div>
+  )
+}
+
+export default EmptyState
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,17 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import { Nunito } from 'next/font/google';
+import { Nunito } from "next/font/google";
 import Navbar from "./components/navbar/Navbar";
 import RegisterModal from "./components/modals/RegisterModal";
 import ToasterProvider from "./providers/ToasterProvider";
 import LoginModal from "./components/modals/LoginModal";
 import getCurrentUser from "./actions/getCurrentUser";
 import RentModal from "./components/modals/RentModal";
-
+import ClientOnly from "./components/ClientOnly";
 
 const font = Nunito({
-  subsets: ['latin'],
-})
+  subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
   title: "StayEase",
@@ -23,18 +23,18 @@ export default async function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const currentUser=await getCurrentUser();
+  const currentUser = await getCurrentUser();
   return (
     <html lang="en">
-      <body
-        className={font.className}
-      >
-        <ToasterProvider/>
-        <RentModal/>
-        <LoginModal/>
-        <RegisterModal/>
-        <Navbar currentUser={currentUser}/>
-        {children}
+      <body className={font.className}>
+        <ClientOnly>
+          <ToasterProvider />
+          <RentModal />
+          <LoginModal />
+          <RegisterModal />
+          <Navbar currentUser={currentUser} />
+        </ClientOnly>
+        <div className="pb-20 pt-28">{children}</div>
       </body>
     </html>
   );
