a1c0ef510cb6da1332eaf6e4f7b20d3014b70308
PrathamDwivedi27 <prathamd549@gmail.com>
2024-11-07 18:07:45 +0530

    [Feat]: Complete the cloudinary setup

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -9,6 +9,7 @@ import { FieldValues, useForm } from 'react-hook-form'
 import CountrySelect from '../inputs/CountrySelect'
 import dynamic from 'next/dynamic'
 import Counter from '../inputs/Counter'
+import ImageUpload from '../inputs/ImageUpload'
 
 enum STEPS{
   CATEGORY=0,
@@ -37,7 +38,7 @@ const RentModal = () => {
         guestCount:1,
         roomCount:1,
         bathroomCount:1,
-        imageSrc:'',
+        imageSrc: '',
         title:'',
         price:1,
         description:'',
@@ -49,6 +50,8 @@ const RentModal = () => {
     const guestCount=watch('guestCount');
     const roomCount=watch('roomCount');
     const bathroomCount=watch('bathroomCount');
+    const imageSrc=watch('imageSrc');
+
 
 
     const Map=useMemo(()=>dynamic(()=>import ('../Map'),{
@@ -162,7 +165,24 @@ const RentModal = () => {
       )
     }
 
+    if(step===STEPS.IMAGES){
+      bodyContent=(
+        <div className='flex flex-col gap-8'>
+          <Heading
+            title='Add a photo of your place'
+            subtitle='Show guests how your place looks like!'
+          />
+          <ImageUpload
+           value={imageSrc}
+           onChange={(value)=>setCustomValue('imageSrc',value)}
+          />
+        </div>
+      )
+    }
+
     
+
+
 
   return (
     <Modal
